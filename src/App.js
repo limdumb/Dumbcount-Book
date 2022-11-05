193511cefe5a8e7b443da0fa0b82c339ca573fe5
JI HYEON KANG <rednebula@mfort.co.kr>
2022-11-05 21:40:57 +0900

    [Feature] 버튼 배경 및 글자 색 프로퍼티 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-import {DemoButton} from "./components/DemoButton";
+import { DemoButton } from "./components/DemoButton";
 
 function App() {
-
+  return (
+    <div>
+      <DemoButton
+        name={"커스텀 버튼"}
+        backgroundColor={'blue'}
+        color={'white'}
+      />
+    </div>
+  )
 }
 
 export default App;
