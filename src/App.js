29d677528d1889930336b8e654682ee31754ff8f
limdumb <dlaruddls@nate.com>
2022-11-10 20:53:01 +0900

    [Misc] Git 충돌 해결

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Header } from "./components/Header";
 
 function App() {
-  return <Header></Header>;
+  return <Header></Header>
 }
 
 export default App;
